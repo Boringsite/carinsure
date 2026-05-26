98d3914f4d1a21294dac1f4e568d720a4ee20e04
Boringsite <hamilton_garth@hotmail.com>
2026-05-26 05:40:55 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './CarInsuranceGuide.jsx'
+import App from './InsuranceWizard.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
